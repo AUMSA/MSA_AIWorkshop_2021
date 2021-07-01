80df11e01104f19cb5d8ff763d7c9c1d372aa233
Mohnish Devadiga <mohnish226@yahoo.co.in>
2021-07-01 15:47:42 +1000

    Update app.py

--- QnA Bot using API/resume_bot/app.py
+++ QnA Bot using API/resume_bot/app.py
@@ -4,13 +4,12 @@ import json
 
 app = Flask(__name__)
 
-url = 'https://resume-bot-service.azurewebsites.net/qnamaker/knowledgebases/590d86cf-b7c2-4000-84da-a81cf296c102/generateAnswer'
-end_point = '2d45b479-0c40-4d84-a396-e8cbb8b8dc85'
+url = '<your-url>'
+end_point = '<your-endpoint>'
 
 @app.route("/")
 def hello():
     return render_template('index.html')
-    # return "This is my first flask app"
 
 @app.route("/ask/<text>")
 def ask(text):
@@ -28,7 +27,3 @@ def ask(text):
                 )
     information = json.loads(response.text)
     return information['answers'][0]['answer']
-
-
-# if __name__ == '__main__':
-#     app.run()
